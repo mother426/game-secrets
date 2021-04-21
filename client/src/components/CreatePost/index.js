f870b2a814bba14a24d037417d6b1026161f8a54
Taylor Weeks <taylorlweeks707@gmail.com>
2021-04-21 13:09:06 -0400

    fixing div in createpost index

--- client/src/components/CreatePost/index.js
+++ client/src/components/CreatePost/index.js
@@ -5,20 +5,31 @@ function CreatePost() {
     return (
         <div>
             <h1>Create Player Question</h1>
-            <form>
+        <form>
+
+        </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Player Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Player Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Player Question:</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <label for="exampleFormControlTextarea1">Player Question:</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">Upload</span>
+                    </div>
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="inputGroupFile01">
+                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
-
-</div>
+                    </div>
+        
+            </div>
+            </div>
 </form>
-</div>
-  );
+    );
 }
 
 export default CreatePost;
