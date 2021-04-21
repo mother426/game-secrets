858e0c7b9f7ba0c149598f61603f18f038fffe0d
brandnn07 <brandcarter4@gmail.com>
2021-04-21 13:08:14 -0400

    bugfixing

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = {
     login: (data) => axios.post('/api/user/login', data),
 
-    signup: (data) => axios.post('api/user', data)
+    signup: (data) => axios.post('api/user', data),
 
     logout: () => axios.post('/api/user/logout')
 
