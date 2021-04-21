92cfefcb33b128f11203d657d263e09bc1040d88
John Kim <johnkimmsterr@gmail.com>
2021-04-21 15:14:01 -0400

    search results

---
+++ client/src/components/SearchResults/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+function SearchResults() {
+
+  return (
+    <div className="card">
+      <div className="card-horizontal">
+        <div className="img-square-wrapper">
+          <img/>
+        </div>
+        <div className="card-body">
+          <h4 className="card-title"></h4>
+          <h6>posted by: BestUs3r123</h6>
+          <p className="card-text">
+          </p>
+        </div>
+      </div>
+      <div className="card-footer">
+        <small className="text-muted"></small>
+      </div>
+    </div>
+  );
+}
+
+export default SearchResults;
