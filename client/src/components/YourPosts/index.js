ab854448b5d6dbf234db573bb42535b2a76674ec
brandnn07 <brandcarter4@gmail.com>
2021-04-24 10:54:09 -0400

    mering

--- client/src/components/YourPosts/index.js
+++ client/src/components/YourPosts/index.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../../utils/api';
 import DeletePost from '../DeletePost';
 import './style.css';
 
-function YourPosts(props) {
-  // const localUser = JSON.parse(sessionStorage.getItem("user"));
-  // console.log(localUser.posts)
-  // const [userName, setuserName] = useState({
-  //   post: localUser.posts,
-  // })
+function YourPosts() {
+  const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        api.getPosts()
+            .then(results => {
+                setPosts(results.data);
+            })
+            .catch(err => console.log(err));
+    }, []);
   const handleDelete = async (id) => {
     try {
       console.log(id);
@@ -26,7 +30,7 @@ function YourPosts(props) {
       <h4 className="your-secrets-title">
         Your Secrets
     </h4>
-      {/* {props.yourPosts.map((post) => ( */}
+      {posts.map(post => (
       <div className="card post-card">
         <div className="card-horizontal">
           <div className="img-square-wrapper">
@@ -37,19 +41,19 @@ function YourPosts(props) {
             />
           </div>
           <div className="card-body">
-            <h4 className="card-title">{props.title}</h4>
-            <h6>posted by: {props.author}</h6>
+            <h4 className="card-title">{post.title}</h4>
+            <h6>posted by: {post.author}</h6>
             <p className="card-text">
-              {props.body}
+              {post.body}
             </p>
           </div>
         </div>
-        <DeletePost onClick={() => handleDelete(props._id)} />
+        <DeletePost onClick={() => handleDelete(post._id)} />
         <div className="card-footer">
-          <small className="text-muted">{props.date}</small>
+          <small className="text-muted">{post.date}</small>
         </div>
       </div>
-      {/* ))} */}
+     ))}
     </>
   )
 }
